d78b38ce539058aa615877c0dfc1dd950fcbd3c3
sh-ikromaliyev <250110@newuu.uz>
2026-04-22 22:39:45 +0500

    Update name and ID in HelloJavaFX

--- HelloJavaFX.java
+++ HelloJavaFX.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 public class HelloJavaFX extends Application {
     public void start(Stage stage) {
-        Text name = new Text("Your Full Name");
+        Text name = new Text("Sherbek Ikromaliyev");
         name.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 20));
 
-        Text id = new Text("Student ID");
+        Text id = new Text("Student ID: 100000");
         id.setFill(Color.RED);
 
         VBox vbox = new VBox(10, name, id);
